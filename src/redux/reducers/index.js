3bc2a85edc041508544f1a5f7d65ceb9bace94ea
Kevin Baker <kbaker@atomeq.com>
2018-04-20 19:53:42 -0400

    fix

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux'
 import BooksReducer from './reducer_books'
 
 const rootReducer = combineReducers({
-    // state: (state = {}) => state
    books: BooksReducer 
 });
 
