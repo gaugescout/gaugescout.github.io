78e1089998d1e0ff48bc1f2bd78bcbfdbc1a631a
Kevin Baker <kbaker@atomeq.com>
2018-04-24 18:20:27 -0400

    round trip E2E action/reducer wiht API workign and displaying

--- src/redux/reducers/reducer_device_locations.js
+++ src/redux/reducers/reducer_device_locations.js
@@ -1,7 +1,12 @@
 // State argument is not application state only
 // the state this reducer is responsible for
 export default function (state = null, action) {
-    console.log("Reducer action:", action);
+    console.log("Reducer action:", action.payload);
+    switch (action.type) {
+        case "RECEIVED_DEVICE_LOCATIONS":
+            // let data = action.payload.data;
+            return action.payload.data;
+    }
     return [
         {id:1, address:'18 Chestnut Way', city: 'Lee', state: 'NH'},
         {id:2, address:'19 Chestnut Way', city: 'Lee', state: 'NH'},
